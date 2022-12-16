2338d49f86f4916d8d819fb710edf41f946321fe
Bruce Neco <bruce.neco3@outlook.com>
2022-12-16 09:16:28 -0300

    feat: add grpc server spin up and serving

--- app/main.go
+++ app/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"github.com/bruceneco/vocup-api/api/scrap"
+	"github.com/bruceneco/vocup-api/app/server/scrap"
 	"github.com/bruceneco/vocup-api/app/services"
-	usecases "github.com/bruceneco/vocup-api/app/usecases/scrap"
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"os"
 )
 
 func main() {
-	scrapperService := services.NewScrapperService()
-	scrapUC := usecases.NewScrapUsecase(scrapperService)
 
-	fmt.Println(scrapUC.ScrapTopWords(), len(scrapUC.ScrapTopWords()))
+	// Services
+	log := hclog.Default()
+	scrapperService := services.NewScrapperService()
+	scrapServer := server.NewScrapServer(scrapperService)
+
+	// Servers
+	gs := grpc.NewServer()
+	scrap.RegisterScrapServer(gs, scrapServer)
+
+	// Application start
+	reflection.Register(gs)
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Error("error on listening to port 8080", "error", err)
+		os.Exit(1)
+	}
+	log.Info("server running", "port", lis.Addr())
+	if err = gs.Serve(lis); err != nil {
+		log.Error("fail on serve", "error", err)
+	}
 }
